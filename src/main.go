c07e206f1f08499f613fcb505f7afe30bd514793
Michael Jurkoic <mjjurkoic@gmail.com>
2023-07-22 15:03:11 -0500

    Break root path handler into separate function

--- src/main.go
+++ src/main.go
@@ -13,33 +13,35 @@ import (
 // Server entry point
 func main() {
 	// Handle WebSocket requests to root path
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		// Upgrade connection to WebSocket
-		conn, err := websocket.Accept(w, r, nil)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		defer conn.Close(websocket.StatusInternalError, "Internal Error")
+	http.HandleFunc("/", handleRoot)
+}
 
-		// Set timeout
-		ctx, cancel := context.WithTimeout(r.Context(), time.Second*10)
-		defer cancel()
+func handleRoot(w http.ResponseWriter, r *http.Request) {
+	// Upgrade connection to WebSocket
+	conn, err := websocket.Accept(w, r, nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer conn.Close(websocket.StatusInternalError, "Internal Error")
 
-		// Read request JSON
-		var v interface{}
-		err = wsjson.Read(ctx, conn, &v)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
+	// Set timeout
+	ctx, cancel := context.WithTimeout(r.Context(), time.Second*10)
+	defer cancel()
 
-		// TODO: Handle "EVENT" request
+	// Read request JSON
+	var v interface{}
+	err = wsjson.Read(ctx, conn, &v)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
-		// TODO: Handle "REQ" request
+	// TODO: Handle "EVENT" request
 
-		// TODO: Handle "CLOSE" request
+	// TODO: Handle "REQ" request
 
-		conn.Close(websocket.StatusNormalClosure, "")
-	})
+	// TODO: Handle "CLOSE" request
+
+	conn.Close(websocket.StatusNormalClosure, "")
 }
